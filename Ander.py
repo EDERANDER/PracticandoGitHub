47f8ac1a9f40b70dd772a79b97ae7facea537b87
Eder Ander <ederander123@gmail.com>
2024-11-13 07:58:48 -0500

    Monitorearemos en archivo Ander.py

---
+++ Ander.py
@@ -0,0 +1,16 @@
+def sumar(a,b):
+    cantidad = 7
+    print(a+b)
+    print("Esta suma es muy facil")
+sumar(5,6)
+
+def multiplicar(a,b):
+    return a * b
+multiplicar(3,5)
+    
+
+def multiplicar(a,b):
+    n = int(input("Ingrese numero: "))
+    print(a*b)
+    print()
+multiplicar(3,4)
