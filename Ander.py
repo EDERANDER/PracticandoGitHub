417136608cb8c4eb6f5c471a06d7c406af29e4ef
AnderEder <135076154+AnderEder@users.noreply.github.com>
2024-11-14 13:58:49 -0500

    Agregue comentarios en el codigo

--- Ander.py
+++ Ander.py
@@ -1,13 +1,16 @@
+#este es una funcion para sumar
 def sumar(a,b):
     cantidad = 7
     print(a+b)
     print("Esta suma es muy facil")
 sumar(5,6)
 
+#este es una funcion para multiplicar
 def multiplicar(a,b):
     return a * b
 multiplicar(3,5)
-       
+
+#este es el main principa       
 def main():
     numero1 = int(input("Ingrese numero 1:"))
     numero2 = int(input("Ingrese numero 2:"))
