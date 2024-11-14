058ec40bb61bc1ae35c23fbf2c539f90423ddc48
Eder Ander <ederander123@gmail.com>
2024-11-14 12:42:23 -0500

    borre funcion dividir

--- Ander.py
+++ Ander.py
@@ -7,10 +7,7 @@ sumar(5,6)
 def multiplicar(a,b):
     return a * b
 multiplicar(3,5)
-    
-def dividir(a,b):
-    print(a/b)
-    
+       
 def main():
     numero1 = int(input("Ingrese numero 1:"))
     numero2 = int(input("Ingrese numero 2:"))
