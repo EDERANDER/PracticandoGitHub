1482c7867f6053ce526617d73e37dca4f448d290
AnderEder <135076154+AnderEder@users.noreply.github.com>
2024-11-20 18:21:15 -0500

    Hice el constructor

--- Gerente.java
+++ Gerente.java
@@ -2,8 +2,9 @@ public class Gerente {
     private String nombre;
     private String telefono;
 
-    public Gerente() {
-
+    public Gerente(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
     }
 
     public void informacion() {
